2e0413cce7f2955759ef875d18392bbfee8fefd7
LookerKy <nagaraz07@gmail.com>
2019-10-22 18:38:21 +0900

    add state

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      // throw Error
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      // this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find your permission", "So You need Allow Permission");
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
 
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
