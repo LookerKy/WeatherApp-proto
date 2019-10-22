49dd287232c4e8bcd69fa76ff50a6d401ea920fc
LookerKy <nagaraz07@gmail.com>
2019-10-22 19:07:00 +0900

    add axios

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "54a52fa4980f1e80530f2c4cbe579c51";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   };
   getLocation = async () => {
     try {
@@ -14,7 +23,8 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
-      // this.setState({ isLoading: false });
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find your permission", "So You need Allow Permission");
     }
