d48514dab2c2d6b752dca4d71abe05d085d5e279
LookerKy <nagaraz07@gmail.com>
2019-11-05 15:11:25 +0900

    fixed bug

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class extends React.Component {
     const { data } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    console.log(data);
+
     this.setState({ isLoading: false, temp: data.main.temp });
   };
   getLocation = async () => {
